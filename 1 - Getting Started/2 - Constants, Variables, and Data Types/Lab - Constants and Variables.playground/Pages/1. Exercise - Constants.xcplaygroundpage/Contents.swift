13ccd3a0dd28a1a1150081ae7aa31c908138d246
l1dl1d <javeekh@ssfs.org>
2022-01-26 15:11:11 -0500

    exersize 3

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ print(friends)
 
 
 //:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
-
+print("the above code does not compile because friends is a constant and you cant change a constant")
 
 /*:
 page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
