72314cceb9b208d6487ac5c74779f127879292d6
l1dl1d <javeekh@ssfs.org>
2022-02-02 11:28:17 -0500

    hello word and button on phone

--- hello word/hello word/ViewController.swift
+++ hello word/hello word/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeText(_ sender: UIButton) {helloLabel.text = "this is neat"
+    }
+    
 }
 
