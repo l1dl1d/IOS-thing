3fae83d011c3f6d3df2c7676b23e845f51cd0380
l1dl1d <javeekh@ssfs.org>
2022-02-11 11:19:56 -0500

    worked a little

--- 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -9,8 +9,8 @@ func intruduceMyself(){
 intruduceMyself()
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
-let randomNum = Int.random(in: 0...4)
 func magicEightBall(){
+    let randomNum = Int.random(in: 0...4)
     if randomNum == 1{
         print("youre number one")
     }
@@ -20,11 +20,14 @@ func magicEightBall(){
     else if randomNum == 3{
         print("cooool number")
     }
-    else{
+    else {
         print("FOOOOORE")
     }
 }
-
+magicEightBall()
+magicEightBall()
+magicEightBall()
+magicEightBall()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
