24e5e510e55887cc913e25523e4e740b0ab44e23
l1dl1d <javeekh@ssfs.org>
2022-02-16 11:55:41 -0500

    sweeeeeeaaaat

---
+++ Temperature Converter/Temperature.swift
@@ -0,0 +1,20 @@
+//
+//  Temperature.swift
+//  Temperature Converter
+//
+//  Created by Khalid Javeed on 2/16/22.
+//
+
+import Foundation
+struct temperature{
+    var tempInFahrenheit: Double
+    var tempInCelsius: Double
+    init(fahrenheit: Double){
+        tempInFahrenheit = fahrenheit
+        tempInCelsius = (5.0/9)*(tempInFahrenheit-32)
+    }
+    init(celsius: Double){
+        tempInCelsius = celsius
+        tempInFahrenheit = (celsius*(9.0/5)) + 32
+    }
+}
