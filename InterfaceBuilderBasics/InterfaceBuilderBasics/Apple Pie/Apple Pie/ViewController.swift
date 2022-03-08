207fa7c2d0af51a8c75ec9fd263c05af72018109
l1dl1d <javeekh@ssfs.org>
2022-03-08 11:54:54 -0500

    i just finished doing the actions for the buttons peas and carrots

--- InterfaceBuilderBasics/InterfaceBuilderBasics/Apple Pie/Apple Pie/ViewController.swift
+++ InterfaceBuilderBasics/InterfaceBuilderBasics/Apple Pie/Apple Pie/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
