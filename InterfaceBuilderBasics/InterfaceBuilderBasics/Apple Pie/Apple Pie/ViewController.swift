9258e7965463948dabb77c5b96a3e663bd15b714
l1dl1d <javeekh@ssfs.org>
2022-04-01 08:59:31 -0400

    one error from success

--- InterfaceBuilderBasics/InterfaceBuilderBasics/Apple Pie/Apple Pie/ViewController.swift
+++ InterfaceBuilderBasics/InterfaceBuilderBasics/Apple Pie/Apple Pie/ViewController.swift
@@ -25,9 +25,27 @@ class ViewController: UIViewController {
         let letterString = sender.configuration!.title!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
     }
-    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        }else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        }else {
+            updateUI()
+        }
+        var totalWins = 0{
+            didSet {
+                newRound()
+            }
+        }
+        var totalLosses = 0{
+            didSet{
+                newRound()
+            }
+        }
+    }
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -36,14 +54,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     func newRound(){
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty{
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        }else{
+            enableLetterButtons(false)
+        }
+    }
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
     }
     func updateUI(){
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
         correctWordLabel.text = currentGame.formattedWord
         scoreLabel.text = "Wins: \(totalWins) loses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+        
     }
 
 }
