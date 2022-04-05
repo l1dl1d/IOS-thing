59b87d65ab8670bd342ac49f202a3a7bb6dc0c2e
l1dl1d <javeekh@ssfs.org>
2022-04-05 11:00:45 -0400

    working on optionals

--- 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
@@ -3,13 +3,19 @@
 
  If an app asks for a user's age, it may be because the app requires a user to be over a certain age to use some of the services it provides. Write a function called `checkAge` that takes one parameter of type `String`. The function should try to convert this parameter into an `Int` value and then check if the user is over 18 years old. If he/she is old enough, print "Welcome!", otherwise print "Sorry, but you aren't old enough to use our app." If the `String` parameter cannot be converted into an `Int` value, print "Sorry, something went wrong. Can you please re-enter your age?" Call the function and pass in `userInputAge` below as the single parameter. Then call the function and pass in a string that can be converted to an integer.
  */
-let userInputAge: String = "34e"
+let userInputAge: String = "34"
 func checkAge(ages: String){
-    if let ages2 = ages{
-        print(ages2)
+    if let ages2 = Int(ages){
+        if ages2 >= 18{
+            print("Welcome!")
+        }else if ages2 <= 18{
+            print("Sorry, but you aren't old enough to use our app.")
+        }else if ages = String{
+            print("Sorry, something went wrong. Can you please re-enter your age?")
+        }
     }
 }
-
+print(userInputAge)
 //:  Go back and update your function to return the age as an integer. Will your function always return a value? Make sure your return type accurately reflects this. Call the function and print the return value.
 
 
