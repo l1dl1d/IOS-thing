07f5215284c9a5e185e12b4a5612fb04bd2dea37
l1dl1d <javeekh@ssfs.org>
2022-04-08 15:18:40 -0400

    almost finished optionals assignment

--- 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
@@ -4,18 +4,20 @@
  If an app asks for a user's age, it may be because the app requires a user to be over a certain age to use some of the services it provides. Write a function called `checkAge` that takes one parameter of type `String`. The function should try to convert this parameter into an `Int` value and then check if the user is over 18 years old. If he/she is old enough, print "Welcome!", otherwise print "Sorry, but you aren't old enough to use our app." If the `String` parameter cannot be converted into an `Int` value, print "Sorry, something went wrong. Can you please re-enter your age?" Call the function and pass in `userInputAge` below as the single parameter. Then call the function and pass in a string that can be converted to an integer.
  */
 let userInputAge: String = "34"
-func checkAge(ages: String){
+func checkAge(ages: String)-> Int?{
     if let ages2 = Int(ages){
         if ages2 >= 18{
             print("Welcome!")
-        }else if ages2 <= 18{
+        }else{
             print("Sorry, but you aren't old enough to use our app.")
-        }else if ages = String{
-            print("Sorry, something went wrong. Can you please re-enter your age?")
         }
+        return ages2
+    }else{
+        print("Sorry, something went wrong. Can you please re-enter your age?")
+        return nil
     }
 }
-print(userInputAge)
+checkAge(ages: "34")
 //:  Go back and update your function to return the age as an integer. Will your function always return a value? Make sure your return type accurately reflects this. Call the function and print the return value.
 
 
--- 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/5. Exercise - Failable Initializers.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/5. Exercise - Failable Initializers.xcplaygroundpage/Contents.swift
@@ -4,6 +4,20 @@
  Create a `Computer` struct with two properties, `ram` and `yearManufactured`, where both parameters are of type `Int`. Create a failable initializer that will only create an instance of `Computer` if `ram` is greater than 0, and if `yearManufactured` is greater than 1970, and less than 2020.
  */
 
+struct Computer {
+    var ram: Int
+    var yearManufactured: Int
+    init?(ram: Int, yearManufactured: Int){
+        if ram > 0 {
+            if yearManufactured > 1970 && yearManufactured < 2000{
+                self.ram = ram
+                self.yearManufactured = yearManufactured
+            }
+        }else {
+            return nil
+        }
+    }
+}
 
 //:  Create two instances of `Computer?` using the failable initializer. One instance should use values that will have a value within the optional, and the other should result in `nil`. Use if-let syntax to unwrap each of the `Computer?` objects and print the `ram` and `yearManufactured` if the optional contains a value.
 
