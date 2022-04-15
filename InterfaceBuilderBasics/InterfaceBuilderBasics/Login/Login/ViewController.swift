c7a721039fbfb0fb2e385d043ac762a003dd50b3
l1dl1d <javeekh@ssfs.org>
2022-04-15 11:00:03 -0400

    started step 2 of lab login

---
+++ InterfaceBuilderBasics/InterfaceBuilderBasics/Login/Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Khalid Javeed on 4/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
