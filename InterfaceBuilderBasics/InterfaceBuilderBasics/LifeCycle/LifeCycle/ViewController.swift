81a4e615aa84d36833e202d606292ec5e5739051
l1dl1d <javeekh@ssfs.org>
2022-04-20 15:15:21 -0400

    middle of life cycle

--- InterfaceBuilderBasics/InterfaceBuilderBasics/LifeCycle/LifeCycle/ViewController.swift
+++ InterfaceBuilderBasics/InterfaceBuilderBasics/LifeCycle/LifeCycle/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ViewController - View Did Load")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController - View Will Appear")
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController - View Did Appear")
+        
     }
 
 
