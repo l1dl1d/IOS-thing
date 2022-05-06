41be06486288f5cff32ed2a74a4df216820268c3
l1dl1d <javeekh@ssfs.org>
2022-05-06 11:54:45 -0400

    started making actions for witches

--- InterfaceBuilderBasics/InterfaceBuilderBasics/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ InterfaceBuilderBasics/InterfaceBuilderBasics/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
-class QuestionViewController: UITableViewController {
+class QuestionViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
     @IBOutlet weak var singleStackView: UIStackView!
     @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
     @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
     @IBOutlet weak var rangedStackView: UIStackView!
-    
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     var questions: [Question] = [
       Question(
         text: "Which food do you like the most?",
@@ -48,31 +62,76 @@ class QuestionViewController: UITableViewController {
       )
     ]
     var questionIndex = 0
-
+    var answersChosen: [Answer] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
        }
 
-       func updateUI() {
-           singleStackView.isHidden = true
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton){
+        let currentAnswers = questions[questionIndex].answers
+        switch sender {
+            case singleButton1:
+                answersChosen.append(currentAnswers[0])
+            case singleButton2:
+                answersChosen.append(currentAnswers[1])
+            case singleButton3:
+                answersChosen.append(currentAnswers[2])
+            case singleButton4:
+                answersChosen.append(currentAnswers[3])
+            default:
+                break
+            }
+
+            nextQuestion()
+    }
+    func nextQuestion() {
+        
+    }
+    
+    func updateUI() {
+               singleStackView.isHidden = true
                multipleStackView.isHidden = true
                rangedStackView.isHidden = true
 
-               navigationItem.title = "Question #\(questionIndex + 1)"
 
                let currentQuestion = questions[questionIndex]
-
+               let currentAnswers = currentQuestion.answers
+               let totalProgress = Float(questionIndex) / Float(questions.count)
+           
+               navigationItem.title = "Question #\(questionIndex + 1)"
+               questionLabel.text = currentQuestion.text
+               questionProgressView.setProgress(totalProgress, animated: true)
                switch currentQuestion.type {
                case .single:
-                   singleStackView.isHidden = false
+                   updateSingleStack(using: currentAnswers)
                case .multiple:
-                   multipleStackView.isHidden = false
+                   updateMultipleStack(using: currentAnswers)
                case .ranged:
-                   rangedStackView.isHidden = false
+                   updateRangedStack(using: currentAnswers)
                }
 
        }
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[3].text
+    }
+    func updateRangedStack(using answers: [Answer]) {
+        rangedStackView.isHidden = false
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
+    }
+
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -83,12 +142,12 @@ class QuestionViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
@@ -148,4 +207,4 @@ class QuestionViewController: UITableViewController {
     }
     */
 
-}
+
